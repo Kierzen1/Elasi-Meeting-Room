bcf2de23a648fa78acf51432766906e2951717e1
Kierzen Ivan Jay Paslon Booc <92258013+Kierzen1@users.noreply.github.com>
2024-10-03 13:58:10 +0800

    customer Crud Added

---
+++ ASI.Basecode.Data/Models/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASI.Basecode.Data.Models
+{
+    public class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Key]
+        public int customerId { get; set; }
+        [Required]
+        public string customerName { get; set; }
+        [Required]
+        public string customerEmail { get; set; }
+        [Required]
+        public string role { get; set; }
+        [Required]
+        public string customerPassword { get; set; }
+        [Required]
+        public DateTime DateTimeCreated = DateTime.Now;
+    }
+}
